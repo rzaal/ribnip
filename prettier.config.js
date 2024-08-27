b4ed921aa3e5180c790303e85c07505227165c94
Robin Zaal <rz25897@gmail.com>
2024-08-27 21:07:30 +0200

    feat: pre-commit hook for linting and formatting

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  trailingComma: 'all',
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+};
+
+module.exports = config;
